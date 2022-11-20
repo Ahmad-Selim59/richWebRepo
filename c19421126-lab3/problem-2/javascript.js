002f516a6db7177a1c0816f9275bf82a09850035
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-11-20 23:11:53 +0000

    connecting button to function

---
+++ c19421126-lab3/problem-2/javascript.js
@@ -0,0 +1,9 @@
+let startButton = document.getElementById("startButton");
+/*
+rxjs.fromEvent(startButton, 'click')
+    .subscribe(() => timer()
+);
+*/
+function timer(){
+    console.log("test")
+}
