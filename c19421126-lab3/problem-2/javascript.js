4097e0d4ca84482f6f29b006591e3558ce0f1c83
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-11-20 23:26:01 +0000

    reading in input for time

--- c19421126-lab3/problem-2/javascript.js
+++ c19421126-lab3/problem-2/javascript.js
@@ -4,6 +4,18 @@ rxjs.fromEvent(startButton, 'click')
     .subscribe(() => timer()
 );
 */
+
+let totalTime = 0;
+
 function timer(){
-    console.log("test")
+    //console.log("test")
+
+    //read in inputs
+    let hours = document.getElementById("hours").value;
+    let minutes = document.getElementById("minutes").value;
+    let seconds = document.getElementById("seconds").value;
+
+    //find out how many seconds it is in total
+    totalTime = (hours * 60 * 60) + (minutes * 60) + seconds;
+    console.log(totalTime)
 }
