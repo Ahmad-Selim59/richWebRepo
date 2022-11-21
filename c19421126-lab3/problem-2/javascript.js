38eb7e0335c475d2f4e0a95d44a34989564dfe98
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-11-21 15:26:53 +0000

    facepalm did it wrong way without any subscribes
    wiping out majority of my JS because i didnt use subscribes and did it all wrong

--- c19421126-lab3/problem-2/javascript.js
+++ c19421126-lab3/problem-2/javascript.js
@@ -5,42 +5,11 @@ rxjs.fromEvent(startButton, 'click')
 );
 */
 
+let TimerDiv = document.getElementById("DisplayTimeCountdown");
 let totalTime = 0;
 
 function timer(){
-    //console.log("test")
-
-    //read in inputs
-    let hours = document.getElementById("hours").value;
-    let minutes = document.getElementById("minutes").value;
-    let seconds = document.getElementById("seconds").value;
-
-    //find out how many seconds it is in total
-    totalTime = (hours * 60 * 60) + (minutes * 60) + seconds;
-    //console.log(totalTime)
-
-    let currentDate = new Date();
-    //console.log(currentDate);
-    let hourGoal = Number(currentDate.getHours()) + Number(hours);
-    let minuteGoal = Number(currentDate.getMinutes()) + Number(minutes);
-    let secondGoal = Number(currentDate.getSeconds()) + Number(seconds);
-    /*console.log(hourGoal);
-    console.log(minuteGoal);
-    console.log(secondGoal);*/
-    //this is the time which the timer will stop at
-    let timerGoal = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), hourGoal, minuteGoal, secondGoal);
-    //console.log(timerGoal);
-
-    let currentTime = currentDate.getTime();
-    let timerGoalParsed = timerGoal.getTime();
-    //console.log(currentTime);
-    //console.log(timerGoalParsed);
-    let remaining = timerGoalParsed - currentTime;
-    //console.log(remaining);
-
-    let hoursRemaining = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutesRemaining = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
-    let secondsRemaining = Math.floor((remaining % (1000 * 60)) / 1000);
-    console.log("hours remaining", hoursRemaining, "minutes remaining", minutesRemaining, "seconds remaining", secondsRemaining);
+    
+   
 
 }
