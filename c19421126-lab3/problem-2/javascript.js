5ce107fb9c96bb2f26c3f085c5412f583939ef5d
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-11-26 22:16:43 +0000

    using rxjs for function
    adding rxjs function

--- c19421126-lab3/problem-2/javascript.js
+++ c19421126-lab3/problem-2/javascript.js
@@ -1,14 +1,16 @@
 let startButton = document.getElementById("startButton");
-/*
-rxjs.fromEvent(startButton, 'click')
-    .subscribe(() => timer()
-);
-*/
+startButton.addEventListener('click', () => {
+    time();
+  });
 
 let TimerDiv = document.getElementById("DisplayTimeCountdown");
 let totalTime = 0;
 
-function timer(){
+
+const time = () => {
+
+};
+
     //read in inputs
     let hours = document.getElementById("hours").value;
     let minutes = document.getElementById("minutes").value;
@@ -17,4 +19,4 @@ function timer(){
     //find out how many seconds it is in total
     totalTime = parseInt(hours * 60 * 60) + parseInt(minutes * 60) + parseInt(seconds);
 
-}
+
