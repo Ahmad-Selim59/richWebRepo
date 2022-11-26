d04d5de1090590dd6e964e5fe74a62de43413525
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-11-26 22:19:20 +0000

    putting stuff inside the function
    taking everything that was outside the function and cutting inside the function

--- c19421126-lab3/problem-2/javascript.js
+++ c19421126-lab3/problem-2/javascript.js
@@ -6,17 +6,20 @@ startButton.addEventListener('click', () => {
 let TimerDiv = document.getElementById("DisplayTimeCountdown");
 let totalTime = 0;
 
-
+let interval;
 const time = () => {
-
-};
-
+    clearInterval(interval);
     //read in inputs
     let hours = document.getElementById("hours").value;
     let minutes = document.getElementById("minutes").value;
     let seconds = document.getElementById("seconds").value;
-
     //find out how many seconds it is in total
     totalTime = parseInt(hours * 60 * 60) + parseInt(minutes * 60) + parseInt(seconds);
+
+};
+
+    
+
+    
 
 
