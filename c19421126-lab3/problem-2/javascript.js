75420a0692b8935b94821801b55d4e17dfdbe471
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-11-28 22:11:18 +0000

    one timer at a time
    added if statement that prevents multiple timers from ticking at the same time, cant start timer while its running. Also cleaned up code by removing unused lines

--- c19421126-lab3/problem-2/javascript.js
+++ c19421126-lab3/problem-2/javascript.js
@@ -17,30 +17,31 @@ const {
   let initialValue = 0;
   let currentValue = 0;
   
-  //let interval;
   const time = () => {
-      clearInterval(interval);
-      //read in inputs
-      let hours = document.getElementById("hours").value;
-      let minutes = document.getElementById("minutes").value;
-      let seconds = document.getElementById("seconds").value;
-      //find out how many seconds it is in total
-      totalTime = parseInt(hours * 60 * 60) + parseInt(minutes * 60) + parseInt(seconds);
-  
-      currentValue = totalTime;
 
-      let endTimer = timerInterval.pipe(takeWhile(() => currentValue != 0));
-      endTimer.subscribe(function () {
-        // minus total amount of seconds
-        currentValue--;
+    if(currentValue == 0){
+        //read in inputs
+        let hours = document.getElementById("hours").value;
+        let minutes = document.getElementById("minutes").value;
+        let seconds = document.getElementById("seconds").value;
+        //find out how many seconds it is in total
+        totalTime = parseInt(hours * 60 * 60) + parseInt(minutes * 60) + parseInt(seconds);
+    
+        currentValue = totalTime;
 
-        //converting time display into hours, minutes and seconds 
-        let hourRemaining = Math.floor(currentValue / 3600);
-        let minuteRemaining = Math.floor(currentValue % 3600 / 60);
-        let secondRemaining = Math.floor(currentValue % 3600 % 60);
-        displayTimer = hourRemaining + " : " + ("0" + minuteRemaining).slice(-2) + " : " + ("0" + secondRemaining).slice(-2) ;
-        
-        TimerDiv.innerHTML = displayTimer;
-    })
+        let endTimer = timerInterval.pipe(takeWhile(() => currentValue != 0));
+        endTimer.subscribe(function () {
+            // minus total amount of seconds
+            currentValue--;
+
+            //converting time display into hours, minutes and seconds 
+            let hourRemaining = Math.floor(currentValue / 3600);
+            let minuteRemaining = Math.floor(currentValue % 3600 / 60);
+            let secondRemaining = Math.floor(currentValue % 3600 % 60);
+            displayTimer = hourRemaining + " : " + ("0" + minuteRemaining).slice(-2) + " : " + ("0" + secondRemaining).slice(-2) ;
+            
+            TimerDiv.innerHTML = displayTimer;
+        })
+    }
   
   };
