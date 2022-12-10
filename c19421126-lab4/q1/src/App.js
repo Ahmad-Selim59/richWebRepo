f3010d889af34b6b007d242f95774769cf8caed0
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-12-10 17:38:32 +0000

    reading in input from user
    reads in user input in text field and outputs it to console

--- c19421126-lab4/q1/src/App.js
+++ c19421126-lab4/q1/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [userInput, setuserInput] = useState("");
+
+  //on change function
+  function handleChange(event) {
+    console.log(event.target.value);
+    setuserInput(event.target.value);
+  }
+
   return (
-    <div className="App">
+    <div>
+      <input onChange={handleChange} type={"text"} placeholder={"Enter a Note Here"}></input>
+      
       
     </div>
   );
