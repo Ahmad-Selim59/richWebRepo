a7d97b2b3c38ba91eb779d3adbdec208d5f40b17
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-12-10 19:10:40 +0000

    no empty notes
    making it so user cant create an empty note

--- c19421126-lab4/q1/src/App.js
+++ c19421126-lab4/q1/src/App.js
@@ -9,15 +9,19 @@ const App = () => {
   const [id, setId] = useState(0);
 
   const addNote = () => {
-    let addedNote = {
-      noteText: userInput, 
-      id: id,
-      color: "white"
+    if(userInput === ""){
+      alert("note cant be blank");
+    }else{
+      let addedNote = {
+        noteText: userInput, 
+        id: id,
+        color: "white"
+      }
+      setId(id + 1)
+      // cannot use push, use spread operator [...oldarray, newelement]
+      setNote(note => [...note, addedNote])
+      console.log(notes)
     }
-    setId(id + 1)
-    // cannot use push, use spread operator [...oldarray, newelement]
-    setNote(note => [...note, addedNote])
-    console.log(notes)
   }
 
   const DeleteNote = (noteId) => {
