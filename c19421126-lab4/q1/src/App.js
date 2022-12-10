b9a61b50d3a7a7eab195e38b9306eadf91a6f4f6
Ahmad-Selim59 <ahmad22selim@gmail.com>
2022-12-10 18:26:20 +0000

    saving notes
    reads in input from the user and saves it in an object still doesnt output the notes

--- c19421126-lab4/q1/src/App.js
+++ c19421126-lab4/q1/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import React, { useState } from 'react';
+import Note from './Note';
 
 const App = () => {
 
   const [userInput, setuserInput] = useState("");
+  const [notes, setNote] = useState([]);
+  const [id, setId] = useState(0);
+
+  const addNote = () => {
+    let addedNote = {
+      noteText: userInput, 
+      id: id,
+      color: "white"
+    }
+    setId(id + 1)
+    // cannot use push, use spread operator [...oldarray, newelement]
+    setNote(note => [...note, addedNote])
+    console.log(notes)
+  }
 
   //on change function
   function handleChange(event) {
@@ -14,7 +29,7 @@ const App = () => {
   return (
     <div>
       <input onChange={handleChange} type={"text"} placeholder={"Enter a Note Here"}></input>
-      
+      <input type={"button"} value={"Add Note"} onClick={addNote}></input>
       
     </div>
   );
---
+++ c19421126-lab4/q1/src/Note.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Note = (props) => {
+
+    return(
+        <div>
+            <input type={"button"} value={"Edit Note"}></input>
+            <input type={"button"} value={"Delete Note"}></input>
+        </div>
+    )
+}
+
+
+export default Note;
